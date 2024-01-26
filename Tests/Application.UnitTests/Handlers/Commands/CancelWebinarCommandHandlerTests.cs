cb38bd368b5b0d65c384bb80564c3dee1d939805
razvancanuci <99394985+razvancanuci@users.noreply.github.com>
2024-01-26 22:08:29 +0200

    Add unit tests

---
+++ Tests/Application.UnitTests/Handlers/Commands/CancelWebinarCommandHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using Application.Handlers.Commands;
+using Application.Requests;
+using Application.Services.Interfaces;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using Domain.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Application.UnitTests.Handlers.Commands;
+
+public class CancelWebinarCommandHandlerTests : RequestHandlerTestsBase<CancelWebinarCommandHandler>
+{
+    private readonly Mock<ICacheService> _cacheServiceMock;
+    protected override CancelWebinarCommandHandler Handler { get; }
+    
+    public CancelWebinarCommandHandlerTests()
+    {
+        _cacheServiceMock = Fixture.Freeze<Mock<ICacheService>>();
+        Handler = Fixture.Create<CancelWebinarCommandHandler>();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task Handle_Returns_NotFoundResult(CancelWebinarRequest request)
+    {
+        // Act
+        var result = await Handler.Handle(request, CancellationToken.None);
+        
+        // Assert
+        result.Should().BeOfType<NotFoundObjectResult>();
+    }
+    
+    [Theory]
+    [AutoData]
+    public async Task Handle_Returns_NoContentResult_PassedThroughCacheDelete(CancelWebinarRequest request)
+    {
+        // Arrange
+        WebinarRepositoryMock.Setup(x => x.GetAsync(
+            w => w.Id == request.WebinarId,
+            null,
+            false
+        )).ReturnsAsync(new List<Webinar> { new Webinar { Id = request.WebinarId }});
+        
+        // Act
+        var result = await Handler.Handle(request, CancellationToken.None);
+        
+        // Assert
+        _cacheServiceMock.Verify(m => m.DeleteKeyAsync(request.KeyToDelete));
+        result.Should().BeOfType<NoContentResult>();
+    }
+}
---
+++ Tests/Application.UnitTests/Services/MessageServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Application.Services.Implementations;
+using Application.Services.Interfaces;
+using Domain.Entities;
+using Domain.Messages;
+using MassTransit;
+using Moq;
+
+namespace Application.UnitTests.Services;
+
+public class MessageServiceTests
+{
+    private readonly Mock<IPublishEndpoint> _publisherMock;
+
+    private readonly IMessageService _sut;
+    public MessageServiceTests()
+    {
+        _publisherMock = new Mock<IPublishEndpoint>();
+        _sut = new MessageService(_publisherMock.Object);
+    }
+
+    [Fact]
+    public async Task Send_PassedThroughPublishEndpoint()
+    {
+        // Act
+        await _sut.Send(new EmailCancellationMessage(new List<Person>()));
+        
+        // Assert
+        _publisherMock.Verify(m => m.Publish(It.IsAny<EmailCancellationMessage>(), It.IsAny<CancellationToken>()));
+    }
+}
