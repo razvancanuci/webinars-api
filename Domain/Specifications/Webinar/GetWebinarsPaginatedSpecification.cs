01943ae6f62aae1b6147c169bbdb550b7d0b7921
razvancanuci <99394985+razvancanuci@users.noreply.github.com>
2024-02-26 22:59:25 +0200

    create specifications and the builder

---
+++ Domain/Specifications/Webinar/GetWebinarsPaginatedSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Constants;
+using Domain.Interfaces.Requests;
+
+namespace Domain.Specifications.Webinar;
+
+public class GetWebinarsPaginatedSpecification : Specification<Entities.Webinar>
+{
+    public GetWebinarsPaginatedSpecification(IPaginatedRequest request)
+    : base(criteria: entity => entity.ScheduleDate > WebinarConstants.AvailabilityDate,
+        orderByAscending: webinar => webinar.ScheduleDate,
+        select: w => new Entities.Webinar {Id = w.Id,
+            Description = w.Description,
+            Host = w.Host,
+            Title = w.Title,
+            ScheduleDate = w.ScheduleDate},
+        pagination: request,
+        asNoTracking: true) { }  
+}
---
+++ Infrastructure/DataAccess/Specifications/SpecificationBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.Extensions;
+using Domain.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Specifications;
+
+public class SpecificationBuilder
+{
+    public static IQueryable<T> Build<T>(IQueryable<T> inputQuery, Specification<T> specification)
+    where T:Entity
+    {
+        var query = inputQuery;
+
+        if (specification.Criteria is not null)
+        {
+            query = query.Where(specification.Criteria);
+        }
+        
+        if (specification.AsNoTracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        if (specification.OrderByAscending is not null)
+        {
+            query = query.OrderBy(specification.OrderByAscending);
+        }
+
+        if (specification.Pagination is not null)
+        {
+            query = query.PageBy(specification.Pagination);
+        }
+        
+        return query;
+    }
+}
