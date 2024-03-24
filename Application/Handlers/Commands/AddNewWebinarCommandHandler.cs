26caded28ea70fecfc2746c742b7ff86c694f4a2
razvancanuci <99394985+razvancanuci@users.noreply.github.com>
2024-03-24 19:38:43 +0200

    Add content service into new webinar handler

--- Application/Handlers/Commands/AddNewWebinarCommandHandler.cs
+++ Application/Handlers/Commands/AddNewWebinarCommandHandler.cs
@@ -10,14 +10,28 @@ namespace Application.Handlers.Commands;
 public class AddNewWebinarCommandHandler : RequestHandlerBase, ICommandHandler<NewWebinarRequest>
 {
     private readonly IFileStorage _fileStorage;
+    private readonly IContentModerationService _contentModerationService;
 
-    public AddNewWebinarCommandHandler(IUnitOfWork unitOfWork, IFileStorage fileStorage) : base(unitOfWork)
+    public AddNewWebinarCommandHandler(
+        IUnitOfWork unitOfWork,
+        IFileStorage fileStorage,
+        IContentModerationService contentModerationService) : base(unitOfWork)
     {
         _fileStorage = fileStorage;
+        _contentModerationService = contentModerationService;
     }
-    
+
     public async Task<IResult> Handle(NewWebinarRequest request, CancellationToken cancellationToken)
     {
+        var stream = request.Image.OpenReadStream();
+
+        var isRacyOrAdult = await _contentModerationService.IsRacyOrAdultImage(stream);
+
+        if (isRacyOrAdult)
+        {
+            return Results.BadRequest("The image contains adult or racy content");
+        }
+        
         var webinar = new Webinar
         {
             Title = request.Title,
@@ -25,17 +39,14 @@ public class AddNewWebinarCommandHandler : RequestHandlerBase, ICommandHandler<N
             Host = request.Host,
             ScheduleDate = request.DateScheduled
         };
-        
+
         await UnitOfWork.WebinarRepository.InsertAsync(webinar);
         await UnitOfWork.SaveAsync();
         
-        if (request.Image is not null)
-        {
-            var extension = WebinarConstants.AcceptedImageExtensions.First(x => request.Image.FileName.EndsWith(x));
-            
-            await _fileStorage.CreateAsync($"{webinar.Id}{extension}", request.Image);
-        }
-        
+        var extension = WebinarConstants.AcceptedImageExtensions.First(x => request.Image.FileName.EndsWith(x));
+
+        await _fileStorage.CreateAsync($"{webinar.Id}{extension}", request.Image);
+
         return Results.Created("AddWebinar", request);
     }
 }
