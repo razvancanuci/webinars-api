47cafc67d40e857ddf72b30075e86f6a14e7a049
razvancanuci <99394985+razvancanuci@users.noreply.github.com>
2024-05-03 22:51:46 +0300

    add new cancel handler test

--- Tests/Application.UnitTests/Handlers/Commands/CancelWebinarCommandHandlerTests.cs
+++ Tests/Application.UnitTests/Handlers/Commands/CancelWebinarCommandHandlerTests.cs
@@ -4,6 +4,8 @@ using Application.Services.Interfaces;
 using AutoFixture;
 using AutoFixture.Xunit2;
 using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Messages;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
@@ -13,11 +15,14 @@ namespace Application.UnitTests.Handlers.Commands;
 public class CancelWebinarCommandHandlerTests : RequestHandlerTestsBase<CancelWebinarCommandHandler>
 {
     private readonly Mock<ICacheService> _cacheServiceMock;
+    private readonly Mock<IMessageService> _messageServiceMock;
     protected override CancelWebinarCommandHandler Handler { get; }
     
     public CancelWebinarCommandHandlerTests()
     {
         _cacheServiceMock = Fixture.Freeze<Mock<ICacheService>>();
+        _messageServiceMock = Fixture.Freeze<Mock<IMessageService>>();
+        
         Handler = Fixture.Create<CancelWebinarCommandHandler>();
     }
 
@@ -49,6 +54,25 @@ public class CancelWebinarCommandHandlerTests : RequestHandlerTestsBase<CancelWe
         
         // Assert
         _cacheServiceMock.Verify(m => m.DeleteKeyAsync(request.KeyToDelete, It.IsAny<CancellationToken>()));
+        result.Should().BeOfType<NoContent>();
+    }
+    
+    [Theory]
+    [AutoData]
+    public async Task Handle_Returns_NoContentResult_PassedThroughMessageService(CancelWebinarRequest request)
+    {
+        // Arrange
+        WebinarRepositoryMock.Setup(x => x.GetByIdAsync(
+            request.WebinarId)).ReturnsAsync( new Webinar
+        {
+            Id = request.WebinarId, PeopleRegistered = new List<Person>{new Person()}
+        });
+        
+        // Act
+        var result = await Handler.Handle(request, CancellationToken.None);
+        
+        // Assert
+        _messageServiceMock.Verify(m => m.Send(It.IsAny<EmailCancellationMessage>(), It.IsAny<CancellationToken>()));
         result.Should().BeOfType<NoContent>();
     }
 }
